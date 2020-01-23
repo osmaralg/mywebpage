ee54b7a639fb8181ed766ba29dbd0734e9617dbc
osmar alcala <osmar.alcala94@gmail.com>
2020-01-23 17:08:17 +0100

    [ADD] files to the different views

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -4,4 +4,17 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
+	url(r'^academic/$', views.academic, name='academic'),
+	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^profesional/$', views.profesional, name='profesional'),
+	url(r'^personal/$', views.personal, name='personal'),
+	url(r'^predictive/$', views.predictive, name='predictive'),
+	url(r'^algorithm/$', views.algorithm, name='algorithm'),
+	url(r'^optimization/$', views.optimization, name='optimization'),
+	url(r'^opinion/$', views.opinion, name='opinion'),
+	url(r'^spatial/$', views.spatial, name='spatial'),
+	url(r'^travel/$', views.travel, name='travel'),
+	url(r'^places/$', views.places, name='places'),
+	url(r'^album/$', views.album, name='album'),
+	url(r'^machine/$', views.machine, name='machine'),
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -3,3 +3,41 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
     
+def academic(request):
+    return render(request, 'academic.html', {})
+
+def profesional(request):
+    return render(request, 'profesional.html', {})
+
+def personal(request):
+    return render(request, 'personal.html', {})
+
+def predictive(request):
+    return render(request, 'predictive.html', {})
+
+def algorithm(request):
+    return render(request, 'algorithm.html', {})
+
+def optimization(request):
+    return render(request, 'optimization.html', {})
+
+def machine(request):
+    return render(request, 'machine.html', {})
+
+def spatial(request):
+    return render(request, 'spatial.html', {})
+
+def travel(request):
+    return render(request, 'travel.html', {})
+
+def places(request):
+    return render(request, 'places.html', {})
+
+def opinion(request):
+    return render(request, 'opinion.html', {})
+
+def album(request):
+    return render(request,  'album.html', {})
+
+def contact(request):
+    return render(request,  'contact.html', {})
